eadc856cde71ae140f1187a3d8474866f84fb2ed
Daniel <danielwinther@hotmail.dk>
2018-02-25 00:53:20 +0100

    Refactor search bar

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Apache Directory Archive
-This theme is inspired by [Apaxy](https://github.com/oupala/apaxy). The icons for the various file extensions are copied from [Apaxy](https://github.com/oupala/apaxy).
+This theme is inspired by [Apaxy](https://github.com/oupala/apaxy).
 
 ## Setup
-1. Download or clone this repository. Copy the `theme` folder and paste it in the root of your site.
+1. Download or clone this repository. Copy the `theme` directory and paste it in the root of your own site.
 2. Copy the `.htaccess` file and paste in the root too.
-3. Upload your own files and folders to the root folder.
+3. Upload your own files and directories to the root directory. They will now be indexed as rows in a table.
 
 ## Theme features
 This theme is setup with Bootstrap's table classes and grid system, and has another few functionalities including:
 * Fancybox for viewing images and PDF files in the browser.
-* Breadcrumb navigation for easy navigation between folders.
-* Search bar that makes all files and directories searchable.
+* Breadcrumb navigation for easy navigation between directories.
+* Search bar that makes all files and directories searchable. This search does not however scan for files inside the directories.
 * Bootstrap CSS styles.
 * **(OPTIONAL)** One way to restrict access to your archive is to use basic authentication for your domain. Use the following Apache configuration in your `.htaccess` file to enable this feature:
 ```
@@ -20,9 +20,10 @@ AuthUserFile /etc/apache2/.htpasswd
 Require valid-user
 ```
 
-**Notice** that you will also need to create a `.htpasswd` file for the authentication to work. This file will contain a username and a password. Use [this tool](http://www.htaccesstools.com/htpasswd-generator/) to generate an entry for the `.htpasswd` file. Finally, remember to change the `AuthUserFile` to the correct path for your server. Whenever you access the domain, you should now be prompted with a login dialog.
+**Notice** that you will also need to create a `.htpasswd` file for the authentication to work. This file will contain a username and an encrypted password. Use [this tool](http://www.htaccesstools.com/htpasswd-generator) to generate an entry for the `.htpasswd` file and upload it to your server.
+Finally, remember to change the `AuthUserFile` to the correct path for where `.htpasswd` is stored on your server. Whenever you access the domain, you should now be prompted with a login dialog.
 
 ## Screenshots
-The following shows screenshots of the archive in action (I use it mostly for vacation photos, degree certificates etc.).
-![Screenshot](Screenshots/screenshot1.png)
-![Screenshot Fancybox](Screenshots/screenshot2.png)
+The following shows screenshots of the archive in use (I use it mostly for vacation photos, degree certificates, software etc.).
+![Screenshot overview](Images/Screenshots/screenshot1.png)
+![Screenshot Fancybox](Images/Screenshots/screenshot2.png)
--- theme/js/custom.js
+++ theme/js/custom.js
@@ -1,53 +1,82 @@
 function manipulateTable(element) {
-	element.addClass('table table-hover table-responsive');
-	$('.table tr').eq(1).remove();
-	$('.table tr').last().remove();
-	$('.table td').removeAttr('align');
+    element.addClass('table table-hover table-responsive');
+    $('.table tr').eq(1).remove();
+    $('.table tr').last().remove();
+    $('.table td').removeAttr('align');
 }
 
-function search(element) {
-	element.on('keyup', function() {
-		var searchString = $(this).val().toLowerCase();
-		$('.table tr').not(':nth-child(1)').not(':nth-child(2)').filter(function() {
-			$(this).toggle($(this).text().toLowerCase().indexOf(searchString) > -1)
-		});
-	});
+function searchBar(element) {
+    element.val(location.hash.substr(1));
+    $('.table tr').not(':nth-child(1)').not(':nth-child(2)').filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(location.hash.substr(1)) > -1);
+    });
+    element.on('keyup', function() {
+        var searchString = $(this).val().toLowerCase();
+        location.hash = searchString;
+        $('.table tr').not(':nth-child(1)').not(':nth-child(2)').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(location.hash.substr(1)) > -1);
+        });
+    });
 }
 
 function generateBreadcrumbs(hostname, path, rootDirectory, element) {
-	var breadcrumbs = path.split('/').slice(1, -1);
-	var concatenateBreadcrumbs = '';
-	var rootDirectoryLi = $('<li>');
-	var rootDirectoryA = $('<a>').attr('href', hostname).text(rootDirectory);
-	element.append(rootDirectoryLi.append(rootDirectoryA));
-	$.each(breadcrumbs, function(key, value) {
-		var breadcrumb = decodeURIComponent(value);
-		concatenateBreadcrumbs += breadcrumb + '/';
-		var li = $('<li>');
-		var a = $('<a>').attr('href', hostname + '/' + concatenateBreadcrumbs).text(breadcrumb);
-		element.append(li.append(a));
-	});
-	var lastA = $(element).find('li > a').last();
-	$(element).find('li').last().text(lastA.text()).addClass('active');
+    var breadcrumbs = path.split('/').slice(1, -1);
+    var concatenateBreadcrumbs = '';
+    var rootDirectoryLi = $('<li>');
+    var rootDirectoryA = $('<a>').attr('href', hostname).text(rootDirectory);
+    element.append(rootDirectoryLi.append(rootDirectoryA));
+    $.each(breadcrumbs, function(key, value) {
+        var breadcrumb = decodeURIComponent(value);
+        concatenateBreadcrumbs += breadcrumb + '/';
+        var li = $('<li>');
+        var a = $('<a>').attr('href', hostname + '/' + concatenateBreadcrumbs).text(breadcrumb);
+        element.append(li.append(a));
+    });
+    var lastA = $(element).find('li > a').last();
+    $(element).find('li').last().text(lastA.text()).addClass('active');
 }
 
-function initializeFancybox(element, options = null) {
-	element.fancybox(options);
-	var table = '.table ';
-	$(table + 'a:contains(".jpg"),' + table + 'a:contains(".jpeg"),' + table + 'a:contains(".png"),' + table + 'a:contains(".gif")' + table + 'a:contains(".bmp")' + table + 'a:contains(".tiff")').each(function() {
-		$(this).attr('data-fancybox', 'image').attr('data-caption', this.text);
-	});
-	$(table + 'a:contains(".pdf")').each(function() {
-		$(this).attr('data-fancybox', 'pdf').attr('data-caption', this.text);
-	});
+function initializeFancybox(element, fileExtensions, options = null) {
+    element.fancybox(options);
+    $.each(fileExtensions, function(key, fileExtension) {
+        if (fileExtension.images) {
+            $.each(fileExtension.images, function(key, images) {
+                $('.table a:contains(".' + images + '")').each(function() {
+                    $(this).attr('data-fancybox', 'fancybox-image').attr('data-caption', this.text);
+                });
+            });
+        }
+        if (fileExtension.iframes) {
+            $.each(fileExtension.iframes, function(key, iframe) {
+                $('.table a:contains(".' + iframe + '")').each(function() {
+                    $(this).attr('data-fancybox', 'fancybox-iframe').attr('data-caption', this.text);
+                });
+            });
+        }
+    });
 }
 
-function footerInformation(element, date) {
-	element.find('#date').text(date.getFullYear());
+function footerInformation(element) {
+    var date = new Date();
+    var title = $('head').find('title').text();
+    element.find('small').text(date.getFullYear() + ' | ' + title);
 }
 
+var fileExtensions = [{
+    'images': [
+    'jpg',
+    'jpeg',
+    'png',
+    'gif',
+    'bmp',
+    'tiff'
+    ],
+    'iframes': [
+    'pdf'
+    ]
+}];
 manipulateTable($('table'));
-search($('#search'));
+searchBar($('#search'));
 generateBreadcrumbs(location.origin, location.pathname, 'Root', $('#breadcrumbs'));
-initializeFancybox($('[data-fancybox]'));
-footerInformation($('footer'), new Date());
+initializeFancybox($('[data-fancybox]'), fileExtensions);
+footerInformation($('footer'));
